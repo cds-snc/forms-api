809026e3115e77014727c2868cd3035aff92f1e7
Wissam Moussa <wissam.moussa@cds-snc.ca>
2024-07-18 22:31:56 -0400

    enable openai

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+import express from 'express';
+import { initialize } from 'express-openapi';
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
+
+const app = new express();
+const port = process.env.PORT || 3000;
+
+initialize({
+  app,
+  docsPath: "/api-definition",
+  exposeApiDocs: true,
+  apiDoc: "./api/v1/api-definition-base.yml",
+  paths: "./api/v1/routes",
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log("Server Listening on port:", port);
+});
+
+app.get("/status", (request, response) => {
+  const status = {
+    "Status": "Running"
+  };
+
+  response.send(status);
 });
