5314241e9fca12aa8bc8330d082a9d9b9be2b9cc
Clément JANIN <ninjaclem8@hotmail.fr>
2024-08-09 14:04:32 -0400

    Update src/middleware/globalErrorHandler/middleware.ts
    Co-authored-by: Pat Heard <patrick.heard@cds-snc.ca>

--- src/middleware/globalErrorHandler/middleware.ts
+++ src/middleware/globalErrorHandler/middleware.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 
 function main(error: Error, _request: Request, response: Response, _next: NextFunction) {
-  console.log(JSON.stringify(error, Object.getOwnPropertyNames(error)));
+  console.error(JSON.stringify(error, Object.getOwnPropertyNames(error)));
   response.sendStatus(500);
 }
 
