3230140d13844d89785504952a1491d0fabf578b
Wissam Moussa <wissam.moussa@cds-snc.ca>
2024-08-13 09:20:14 -0400

    format

--- utilTokenGeneration.mjs
+++ utilTokenGeneration.mjs
@@ -1,18 +1,18 @@
 /*
-* This script is used to generate a JWT token and exchange it for an opaque token
-*/
-import { SignJWT, importPKCS8 } from 'jose';
-import { createRequire } from 'node:module';
-import 'dotenv/config'
-import axios from 'axios';
+ * This script is used to generate a JWT token and exchange it for an opaque token
+ */
+import { SignJWT, importPKCS8 } from "jose";
+import { createRequire } from "node:module";
+import "dotenv/config";
+import axios from "axios";
 
 const require = createRequire(import.meta.url);
 
 const main = async () => {
   try {
-    const { key, keyId, userId } = require('./clzctlsjk00001y2t0o7dwnv9_private_api_key.json');
+    const { key, keyId, userId } = require("./clzctlsjk00001y2t0o7dwnv9_private_api_key.json");
     const alg = "RS256";
-    const privateKey = await importPKCS8(key, alg)
+    const privateKey = await importPKCS8(key, alg);
 
     const jwt = await new SignJWT()
       .setProtectedHeader({ alg, kid: keyId })
@@ -26,22 +26,23 @@ const main = async () => {
     const reqData = {
       grant_type: "urn:ietf:params:oauth:grant-type:jwt-bearer",
       scope: `openid profile urn:zitadel:iam:org:project:id:${process.env.PROJECT_ID}:aud`,
-      assertion: jwt
-    }
+      assertion: jwt,
+    };
 
-    const token = await axios.post(
-      process.env.ZITADEL_TOKEN_URL, {},
-      {
-        params: reqData,
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
-      }
-    )
+    const token = await axios
+      .post(
+        process.env.ZITADEL_TOKEN_URL,
+        {},
+        {
+          params: reqData,
+          headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        }
+      )
       .catch((e) => {
         console.error(e);
       });
 
     console.log(token.data);
-
   } catch (e) {
     console.log(e);
   }
