6bb178d99d93ed9fc6460145f39e2676ae8086f7
Clément JANIN <ninjaclem8@hotmail.fr>
2024-08-26 07:35:02 -0400

    refactor: remove /downloaded API path (#35)

--- src/middleware/routeNotFound/middleware.ts
+++ src/middleware/routeNotFound/middleware.ts
@@ -1,5 +1,5 @@
 import type { Request, Response } from "express";
 
 export function routeNotFoundMiddleware(_request: Request, response: Response) {
-  response.sendStatus(404);
+  return response.sendStatus(404);
 }
