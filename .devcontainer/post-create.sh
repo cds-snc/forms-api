857907381542b1259e191f4006a989b2e7a4afc8
Pat Heard <patrick.heard@cds-snc.ca>
2024-08-29 18:14:14 +0000

    fix: post create prompt for download

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-corepack enable pnpm
-corepack use pnpm@9.6.0
+export COREPACK_ENABLE_DOWNLOAD_PROMPT=0 
 
+corepack enable pnpm
 pnpm config set store-dir /home/vscode/pnpm/store
+
+corepack use pnpm@9.6.0
 pnpm install
