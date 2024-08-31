6bcfc0b68d3ab891f5577af542f7bcd3b2c98951
Pat Heard <patrick.heard@cds-snc.ca>
2024-08-31 02:36:12 +0000

    fix: prevent infinite reconnect
    Add a reconnect strategy that stops attempting to
    reconnect after a given number of failures.
    
    Update the `getInstance` method so that it does not attempt
    to create multiple connections on first init.

--- src/lib/redisConnector.ts
+++ src/lib/redisConnector.ts
@@ -3,23 +3,31 @@ import { REDIS_URL } from "@src/config";
 
 export class RedisConnector {
   private static instance: RedisConnector | undefined = undefined;
+  private static RETRY_MAX = 5;
+  private static RETRY_DELAY = 1000;
 
   public client: RedisClientType;
 
   private constructor() {
-    this.client = createClient({ url: REDIS_URL });
+    this.client = createClient({
+      url: REDIS_URL,
+      socket: {
+        reconnectStrategy: (retries: number): number | Error =>
+          retries < RedisConnector.RETRY_MAX
+            ? RedisConnector.RETRY_DELAY
+            : new Error("Failed to connect to Redis"),
+      },
+    });
     this.client
-      .on("error", (err) => console.error("Redis client error:", err))
-      .on("reconnecting", () => console.info("Redis client reconnecting..."));
+      .on("error", (err: Error) => console.error("Redis client error:", err))
+      .on("ready", () => console.debug("Redis client ready!"))
+      .on("reconnecting", () => console.debug("Redis client reconnecting..."));
   }
 
   public static async getInstance(): Promise<RedisConnector> {
-    if (
-      RedisConnector.instance === undefined ||
-      !RedisConnector.instance.client.isReady
-    ) {
+    if (RedisConnector.instance === undefined) {
       RedisConnector.instance = new RedisConnector();
-      await RedisConnector.instance.client.connect();
+      RedisConnector.instance.client.connect();
     }
     return RedisConnector.instance;
   }
