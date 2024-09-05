ae4890f38f8d8d9ab5bd88fd0f1ab10302492fae
Clément Janin <clement.janin@cds-snc.ca>
2024-09-05 14:05:42 -0400

    add missing tests

--- test/routes/forms/submission/router.test.ts
+++ test/routes/forms/submission/router.test.ts
@@ -1,20 +1,19 @@
 import { vi, describe, it, expect, beforeAll } from "vitest";
 import request from "supertest";
-import express, { type Express } from "express";
+import express, { Router, type Express, type Response } from "express";
 import { submissionApiRoute } from "@routes/forms/submission/router";
-import { getFormSubmission } from "@src/lib/vault/getFormSubmission";
-import { getNewFormSubmissions } from "@src/lib/vault/getNewFormSubmissions";
-import { FormSubmissionStatus } from "@src/lib/vault/dataStructures/formSubmission";
-import {
-  buildMockedFormSubmission,
-  buildMockedNewFormSubmission,
-} from "test/mocks/formSubmission";
 
-vi.mock("@lib/vault/getFormSubmission");
-const getFormSubmissionMock = vi.mocked(getFormSubmission);
+vi.mock("@routes/forms/submission/new/router", () => ({
+  newApiRoute: Router().get("/", (_, response: Response) => {
+    return response.sendStatus(200);
+  }),
+}));
 
-vi.mock("@lib/vault/getNewFormSubmissions");
-const getNewFormSubmissionsMock = vi.mocked(getNewFormSubmissions);
+vi.mock("@routes/forms/submission/submissionName/router", () => ({
+  submissionNameApiRoute: Router().get("/", (_, response: Response) => {
+    return response.sendStatus(200);
+  }),
+}));
 
 describe("/forms/:formId/submission", () => {
   let server: Express;
@@ -26,12 +25,7 @@ describe("/forms/:formId/submission", () => {
 
   describe("/new", () => {
     it("Response to GET operation", async () => {
-      getNewFormSubmissionsMock.mockResolvedValueOnce([
-        buildMockedNewFormSubmission(),
-      ]);
-
       const response = await request(server).get("/new");
-
       expect(response.status).toBe(200);
     });
   });
@@ -39,12 +33,7 @@ describe("/forms/:formId/submission", () => {
   describe("/:submissionName", () => {
     describe("Response to GET operation when", () => {
       it("submissionName format is valid", async () => {
-        getFormSubmissionMock.mockResolvedValueOnce(
-          buildMockedFormSubmission(FormSubmissionStatus.New),
-        );
-
         const response = await request(server).get("/01-08-a571");
-
         expect(response.status).toBe(200);
       });
 
