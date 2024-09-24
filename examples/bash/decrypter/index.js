b6b4fe8c707a0bab121a749170740ded9899b769
Pat Heard <patrick.heard@cds-snc.ca>
2024-09-24 20:37:58 +0000

    fix: format

--- examples/bash/decrypter/index.js
+++ examples/bash/decrypter/index.js
@@ -1,7 +1,7 @@
 import { createDecipheriv } from "node:crypto";
 
 /**
- * Perform  AES-256-GCM decryption on the encrypted response data.  
+ * Perform  AES-256-GCM decryption on the encrypted response data.
  * @param {string} encryptedResponses Encrypted response data to decrypt.
  * @param {Buffer} key Key that was used to encrypt the responses.
  * @param {Buffer} iv Initialization vector (nonce) that was part of the encryption process.
@@ -9,18 +9,11 @@ import { createDecipheriv } from "node:crypto";
  * @returns Decrypted response data.
  */
 function decryptFormSubmission(encryptedResponses, key, iv, authTag) {
-  const gcmDecipher = createDecipheriv(
-    "aes-256-gcm",
-    key,
-    iv,
-  );
+  const gcmDecipher = createDecipheriv("aes-256-gcm", key, iv);
 
   gcmDecipher.setAuthTag(authTag);
 
-  const encryptedData = Buffer.from(
-    encryptedResponses,
-    "base64",
-  );
+  const encryptedData = Buffer.from(encryptedResponses, "base64");
 
   const decryptedData = Buffer.concat([
     gcmDecipher.update(encryptedData),
@@ -38,5 +31,10 @@ const iv = Buffer.from(args[2], "base64");
 const authTag = Buffer.from(args[3], "base64");
 
 // Decrypt the responses
-const decrypedResponses = decryptFormSubmission(encryptedResponses, key, iv, authTag);
+const decrypedResponses = decryptFormSubmission(
+  encryptedResponses,
+  key,
+  iv,
+  authTag,
+);
 console.info(decrypedResponses);
