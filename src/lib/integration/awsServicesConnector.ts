91fd334e019aaabebfa6c895d13c8f8367206ba7
Clément Janin <clement.janin@cds-snc.ca>
2025-03-20 13:22:15 -0400

    chore: make API service compatible with AWS Scratch account environment

--- src/lib/integration/awsServicesConnector.ts
+++ src/lib/integration/awsServicesConnector.ts
@@ -2,16 +2,10 @@ import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 import { SQSClient } from "@aws-sdk/client-sqs";
 import { SecretsManagerClient } from "@aws-sdk/client-secrets-manager";
-import { AWS_REGION, LOCALSTACK_ENDPOINT } from "@config";
+import { AWS_REGION } from "@config";
 
 const globalConfig = {
   region: AWS_REGION,
-};
-
-const localstackConfig = {
-  ...(LOCALSTACK_ENDPOINT && {
-    endpoint: LOCALSTACK_ENDPOINT,
-  }),
 };
 
 export class AwsServicesConnector {
@@ -27,18 +21,15 @@ export class AwsServicesConnector {
     this.dynamodbClient = DynamoDBDocumentClient.from(
       new DynamoDBClient({
         ...globalConfig,
-        ...localstackConfig,
       }),
     );
 
     this.secretsClient = new SecretsManagerClient({
       ...globalConfig,
-      ...localstackConfig,
     });
 
     this.sqsClient = new SQSClient({
       ...globalConfig,
-      ...localstackConfig,
     });
   }
 
