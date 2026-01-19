a613969ae0f323ddf64b6a43bdf0209e2b412409
Clément Janin <clement.janin@cds-snc.ca>
2026-01-19 14:57:23 -0500

    adding more tests

--- src/middleware/requestContext.ts
+++ src/middleware/requestContext.ts
@@ -1,11 +1,11 @@
 import type { NextFunction, Request, Response } from "express";
 import { requestContextualStore } from "@lib/storage/requestContextualStore.js";
 
-export async function requestContextMiddleware(
+export function requestContextMiddleware(
   _: Request,
   __: Response,
   next: NextFunction,
-): Promise<void> {
+): void {
   try {
     // Sets up the request context store and ensures that all subsequent middleware and route handlers run within that context
     requestContextualStore.run(new Map<string, string>(), () => next());
